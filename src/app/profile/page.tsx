1ff676fb56a39f3721307317d9351fc754043758
akil-metarouge <akil@metarouge.com.au>
2025-09-18 12:17:34 +0600

    seperate authOptions from next-auth route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { CircleUser } from "lucide-react";
+import { authOptions } from "@/lib/authOptions";
 
 export default async function ProfilePage() {
   const session = await getServerSession(authOptions);
